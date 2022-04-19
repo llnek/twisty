22c20fe398a91c6034a942d44c5d8ef3c4f205be
Kenneth Leung <ccllnekk@gmail.com>
2022-04-19 16:48:20 -0700

    updated to latest

--- project.clj
+++ project.clj
@@ -1,28 +1,29 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
-(defproject io.czlab/twisty "2.0.1"
+(defproject io.czlab/twisty "2.1.0"
 
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:url "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            :name "Apache License"}
 
   :description "Useful s/mime, crypto functions"
   :url "https://github.com/llnek/twisty"
 
-  :dependencies [[org.bouncycastle/bcprov-jdk15on "1.64"]
-                 [org.bouncycastle/bcmail-jdk15on "1.64"]
-                 [org.bouncycastle/bcpkix-jdk15on "1.64"]
+  :dependencies [[org.bouncycastle/bcprov-jdk15on "1.70"]
+                 [org.bouncycastle/bcmail-jdk15on "1.70"]
+                 [org.bouncycastle/bcpkix-jdk15on "1.70"]
                  [org.apache.commons/commons-email "1.5"]
-                 [commons-codec/commons-codec "1.13"]
+                 [commons-codec/commons-codec "1.15"]
                  [com.sun.mail/javax.mail "1.6.2"]
                  [org.jasypt/jasypt "1.9.3"]
                  [org.mindrot/jbcrypt "0.4"]
-                 [io.czlab/basal "2.0.1"]]
+                 [io.czlab/basal "2.1.0"]]
 
-  :plugins [[cider/cider-nrepl "0.22.4"]
-            [lein-codox "0.10.7"]]
+  :plugins [[cider/cider-nrepl "0.28.3"]
+            [lein-codox "0.10.8"]
+            [lein-cljsbuild "1.1.8"]]
 
   :profiles {:provided {:dependencies
-                        [[org.clojure/clojure "1.10.1" :scope "provided"]]}
+                        [[org.clojure/clojure "1.11.1" :scope "provided"]]}
              :uberjar {:aot :all}}
 
   :test-selectors {:core :test-core
@@ -40,8 +41,8 @@
   :test-paths ["src/test/clojure"]
 
   :jvm-opts ["-Dlog4j.configurationFile=file:attic/log4j2.xml"]
-  :javac-options ["-source" "11"
-                  "-target" "11"
+  :javac-options ["-source" "16"
+                  "-target" "16"
                   "-Xlint:unchecked" "-Xlint:-options" "-Xlint:deprecation"])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
